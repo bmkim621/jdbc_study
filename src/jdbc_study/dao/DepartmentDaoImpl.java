10e28c5b8f49954029238784cd1a06e4533f346d
bmkim621 <moonstruck0614@gmail.com>
2018-11-09 16:29:49 +0900

    selectDepartmentByAll 수정
    selectDepartmentByAll try-resource-catch문으로 수정
    getDepartment() 메서드 추가

--- src/jdbc_study/dao/DepartmentDaoImpl.java
+++ src/jdbc_study/dao/DepartmentDaoImpl.java
@@ -22,50 +22,24 @@ public class DepartmentDaoImpl implements DepartmentDao {
 		//DB에 select한 결과를 ArrayList로
 		List<Department> list = new ArrayList<>();
 		String sql = "select deptno, deptname, floor from department";
-		
-		//db에 연결,, mysqljdbcutil에서 connection...java.sql로 import
-		Connection conn = null;
-		//PreparedStatement => java.util(인터페이스!)
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		
-		try {
-			conn = MySQLjdbcUtil.getConnection();
-			//sql문장 보낼 준비 하기(prepare)
-			pstmt = conn.prepareStatement(sql);
-			//확인하기
-			LOG.debug(pstmt);
-			//insert 하는 경우 executeQuery, 나머지 update/insert/delet => executeUpdate();
-			rs = pstmt.executeQuery();
-			
-			//반복문(db 데이터 읽기,,,,, 테이블에 데이터 없으면 false)
+		try(Connection conn = MySQLjdbcUtil.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql);
+				ResultSet rs = pstmt.executeQuery()){
 			while(rs.next()) {
-				//getInt("필드명") //인덱스보다는 필드명 쓰는게 좋음!
-				//인덱스 번호 기준은 ★select★ 했을 때의 절을 기준으로 deptno(인덱스 0), deptname(인덱스 1), floor(인덱스 2)임.
-				int deptNo = rs.getInt("deptno");
-				String deptName = rs.getString("deptname");
-				int floor = rs.getInt("floor");
-				
-				Department dept = new Department(deptNo, deptName, floor);
-				
-				//리스트에 넣어준다.
-				list.add(dept);
+				list.add(getDepartment(rs));
 			}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally{
-			//끝나면 반드시 connection 종료("역순"으로 종료시킴)
-			try {
-				rs.close();
-				pstmt.close();
-				conn.close();
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		} catch (SQLException e1) {
+			e1.printStackTrace();
 		}
 		return list;
+	}
+
+	private Department getDepartment(ResultSet rs) throws SQLException {
+		int deptNo = rs.getInt("deptno");
+		String deptName = rs.getString("deptname");
+		int floor = rs.getInt("floor");
+		
+		return new Department(deptNo, deptName, floor);
 	}
 
 }
